5242ad2ed147190461569047ad05f6fa4fb7f2eb
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-05-30 03:43:50 +0700

    fixed error

--- commands/message/ping.js
+++ commands/message/ping.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Check bot latency',
     execute: async (client, message) => {
         const loadMsg = await message.reply({ content: 'Pinging...' });
-        loadMsg.edit({ content: `Pong!: **${wait.createdTimestamp-message.createdTimestamp}** ms` });
+        loadMsg.edit({ content: `Pong!: **${loadMsg.createdTimestamp-message.createdTimestamp}** ms` });
     }
 };
